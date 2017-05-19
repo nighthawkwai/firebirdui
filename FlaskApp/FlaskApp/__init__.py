edaf61002339d42a72230d493e522b80e2ed53c4
Ubuntu <burd@firebirdui.ymizudsjxbzuxalkfsa1oybkva.xx.internal.cloudapp.net>
2017-05-19 03:19:34 +0000

    Added backend support for reject

--- FlaskApp/FlaskApp/__init__.py
+++ FlaskApp/FlaskApp/__init__.py
@@ -1,7 +1,43 @@
-from flask import Flask
+import sqlite3
+import os
+from flask import g
+from flask import Flask, request
+
 app = Flask(__name__)
+DATABASE = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../database/firebirddb.db'))
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
 @app.route("/")
 def hello():
     return "Hellow world"
+
+@app.route("/reject", methods=['POST'])
+def reject():
+    photo_filename = request.form["photo_filename"]
+    username = request.form["username"]
+    db = get_db()
+    db.execute("INSERT OR IGNORE INTO Users (Username) VALUES (?)", [username])
+    db.execute("INSERT INTO Classifications (PhotoId, UserId, FirelineData, WasRejected) VALUES ((SELECT PhotoId FROM Photos WHERE Filename == ?), (SELECT UserId FROM Users WHERE Username == ?), NULL, 1)", [photo_filename, username])
+    db.commit()
+
+    return "Success"
+
 if __name__ == "__main__":
     app.run()
--- FlaskApp/flaskapp.wsgi
+++ FlaskApp/flaskapp.wsgi
@@ -1,9 +1,11 @@
 #!/usr/bin/python
+import os
 import sys
 import logging
 logging.basicConfig(stream=sys.stderr)
 sys.path.insert(0,"/var/www/FlaskApp/")
 
 from FlaskApp import app as application
-with open('../secrets/flask.txt', 'r') as f:
+secrets_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../secrets/flask.txt'))
+with open(secrets_path, 'r') as f:
   application.secret_key = f.readline()
